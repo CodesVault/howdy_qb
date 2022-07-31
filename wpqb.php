c08fb999e22e9a168140895abd49efa2a9ca3844
AbmSourav <keramotul.islam@gmail.com>
2022-07-31 17:04:47 +0600

    package header added

--- wpqb.php
+++ wpqb.php
@@ -2,33 +2,14 @@
 /**
  * @package wpqb
  *
- * Plugin Name: WP Query Builder
- * Plugin URI: 
- * Description: A wordpress starter plugin.
- * Version: 0.0.1
- * Author: CodesVault
- * Author URI: https://github.com/CodesVault
- * License: GPLv2 or later
- * Text Domain: wpqb
+ * @author CodesVault
+ * @version 0.0.1
  */
-
-if ( ! defined( 'ABSPATH' ) ) die();
-
-// use CodesVault\WPqb\SqlEngine;
 
 use CodesVault\WPqb\Connect;
 use CodesVault\WPqb\DB;
-// use CodesVault\WPqb\WPQueryBuilder;
-
-// use function Latitude\QueryBuilder\field;
 
 require_once __DIR__ . '/vendor/autoload.php';
-
-// global $wpdb;
-// $conn = Connect::run();
-// $posts = $conn->prepare(DB::select('*')->from('posts')->get());
-// $posts->execute();
-// print_r($posts->fetchAll());
 
 // $data = DB::select('ID', 'post_title')
 //         ->distinct()
@@ -39,17 +20,18 @@ require_once __DIR__ . '/vendor/autoload.php';
 //         })
 //         ->get();
 
-$data = DB::select('posts.ID', 'posts.post_title')
-        ->from('posts')
-        ->alias('posts')
-        // ->whereIn('ID', 12, 2, 5)
-        ->orderBy(['post_date', 'post_title'], 'ASC')
-        ->limit(4)
-        ->get();
+// $data = DB::select('posts.ID', 'posts.post_title')
+//         ->from('posts')
+//         ->alias('posts')
+//         // ->whereIn('ID', 12, 2, 5)
+//         ->orderBy(['post_date', 'post_title'], 'ASC')
+//         ->limit(4)
+//         ->get();
 
 // echo '<pre>';
 // print_r($data);
 // echo '</pre>';
+
 
 // $host = $_SERVER['HTTP_HOST'];
 // $dns =  'mysql:host=' . $host . ';dbname=' . $wpdb->dbname;
