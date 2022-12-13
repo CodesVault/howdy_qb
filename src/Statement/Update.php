166d946a811008581d7a551ec78df25093f69ddf
AbmSourav <keramotul.islam@gmail.com>
2022-12-13 16:27:21 +0600

    driver support added for update statement

--- src/Statement/Update.php
+++ src/Statement/Update.php
@@ -3,6 +3,7 @@
 namespace CodesVault\Howdyqb\Statement;
 
 use CodesVault\Howdyqb\Api\UpdateInterface;
+use CodesVault\Howdyqb\QueryFactory;
 use CodesVault\Howdyqb\SqlGenerator;
 use CodesVault\Howdyqb\Utilities;
 
@@ -26,15 +27,23 @@ class Update implements UpdateInterface
         $this->sql['set_columns'] = $this->set_columns();
     }
 
+    private function driver_exicute($sql)
+    {
+        $driver = $this->db;
+        if ('wpdb' === QueryFactory::getDriver()) {
+            return $driver->query($driver->prepare($sql, $this->params));
+        }
+
+        $data = $driver->prepare($sql);
+        return $data->execute($this->params);
+    }
+
     private function update_data()
     {
         $query = SqlGenerator::update($this->sql);
-        // return dump($this->params);
 
-        $conn = $this->db;
         try {
-            $data = $conn->prepare($query);
-            return $data->execute($this->params);
+            $this->driver_exicute($query);
         } catch (\Exception $exception) {
             Utilities::throughException($exception);
         }
@@ -57,7 +66,7 @@ class Update implements UpdateInterface
 
         $columns = [];
         foreach ($this->data as $column => $value) {
-            $columns[] = $column . '=?';
+            $columns[] = $column . '=' .  Utilities::get_placeholder();
             $this->params[] = $value;
         }
         return 'SET ' . implode(', ', $columns);
@@ -69,21 +78,21 @@ class Update implements UpdateInterface
             call_user_func( $column, $this );
             return $this;
         }
-        $this->sql['where'] = 'WHERE ' . $column . ' ' . $operator . ' ?';
+        $this->sql['where'] = 'WHERE ' . $column . ' ' . $operator . ' ' . Utilities::get_placeholder();
         $this->params[] = $value;
         return $this;
     }
 
     public function andWhere(string $column, string $operator = null, string $value = null): self
     {
-        $this->sql['andWhere'] = 'AND ' . $column . ' ' . $operator . ' ?';
+        $this->sql['andWhere'] = 'AND ' . $column . ' ' . $operator . ' ' . Utilities::get_placeholder();
         $this->params[] = $value;
         return $this;
     }
 
     public function orWhere(string $column, string $operator = null, string $value = null): self
     {
-        $this->sql['orWhere'] = 'OR ' . $column . ' ' . $operator . ' ?';
+        $this->sql['orWhere'] = 'OR ' . $column . ' ' . $operator . ' ' . Utilities::get_placeholder();
         $this->params[] = $value;
         return $this;
     }
