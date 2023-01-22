c859204a8b1bfa6081a6cb562c02a1aea37fdcd6
AbmSourav <keramotul.islam@gmail.com>
2023-01-22 14:37:48 +0600

    seo meta tags added

--- doc/theme.config.tsx
+++ doc/theme.config.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <strong style={{ color: '#782cfd' }}>WP QueryBuilder</strong>,
@@ -13,6 +15,27 @@ const config: DocsThemeConfig = {
   primaryHue: 260,
   sidebar: {
 	defaultMenuCollapseLevel: 1
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s | WP QueryBuilder'
+    }
+  },
+  head: () => {
+	  const { asPath } = useRouter()
+	  const { title } = useConfig()
+	  return <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	  <meta property="og:url" content={`https://wp-querybuilder.pages.dev${asPath}`} />
+      <meta property="og:title" content={title || 'WP QueryBuilder'} />
+      <meta property="twitter:title" content={title || 'WP QueryBuilder'} />
+      <meta property="og:description" content="Query builder for WordPress" />
+      <meta property="twitter:description" content="Query builder for WordPress" />
+	  <meta property="og:type" content="documentation" />
+	  <meta property="twitter:card" content="documentation" />
+	  <meta property='og:image' content='//abmsourav.com/welcome/wp-content/uploads/2023/01/WP-QueryBuilder.jpg' />
+	  <meta property='twitter:image' content='//abmsourav.com/welcome/wp-content/uploads/2023/01/WP-QueryBuilder.jpg' />
+    </>
   }
 //   banner: {
 //     key: '2.0-release',
