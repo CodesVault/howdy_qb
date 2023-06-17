e9806ef3f992dca9584e83ba8d0f4ab3e13f06d7
Al-Amin Firdows <alaminfirdows@gmail.com>
2023-06-17 12:22:25 +0600

    feat: add test for nullable check

--- tests/CreateTest.php
+++ tests/CreateTest.php
@@ -7,12 +7,11 @@ use CodesVault\Howdyqb\Tests\Statement\CreateApi;
 
 class CreateTest extends TestCase
 {
-    public function testCreateTable()
+    public function test_create_table()
     {
-        $sql = "CREATE TABLE wp_test (ID BIGINT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) DEFAULT 'nil', INDEX (ID) )";
+        $sql = "CREATE TABLE wp_test (ID BIGINT(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) DEFAULT 'nil', INDEX (ID))";
 
-        $query =
-            CreateApi::create('test')
+        $query = CreateApi::create('test')
             ->column('ID')->bigInt()->unsigned()->autoIncrement()->primary()->required()
             ->column('name')->string(255)->required()
             ->column('email')->string(255)->default('nil')
@@ -20,5 +19,23 @@ class CreateTest extends TestCase
             ->getSql();
 
         $this->assertSame($sql, $query);
+    }
+
+    public function test_nullable()
+    {
+        $sql = "CREATE TABLE wp_test (secondary_email VARCHAR(255) DEFAULT 'NULL')";
+
+        $query = CreateApi::create('test')
+            ->column('secondary_email')->string(255)->default('NULL')
+            ->getSql();
+
+        $this->assertSame($sql, $query);
+
+        $query = CreateApi::create('test')
+            ->column('secondary_email')->string(255)->nullable()
+            ->getSql();
+
+        $this->assertSame($sql, $query);
+
     }
 }
